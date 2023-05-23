0f9c74b8a9539877e04d951c734a46ce3a563260
Kariiem <krm.taha@outlook.com>
2023-05-23 03:24:36 +0300

    switch case fix type check

--- src/ast/expression/switch.c
+++ src/ast/expression/switch.c
@@ -5,6 +5,7 @@
 #include "block.h"
 #include "case.h"
 #include "expr.h"
+#include <string.h>
 #include <stdlib.h>
 
 ast_switch_t *create_ast_switch_t(ast_expr_t *expr, vtype(ast_case_t *) cases) {
@@ -36,10 +37,12 @@ void print_ast_switch_t(ast_switch_t const *switch_, int indent) {
 
 void walk_ast_switch_t(ast_switch_t const *switch_, int *id) {
   DEBUG_EPRINTF("walk ast_switch_t\n");
-  if (switch_->enum_expr->type == EXPR_INTEGER ||
-      (switch_->enum_expr->type == EXPR_IDENTIFIER &&
-       !strcmp(get_ast_expr_type(switch_->enum_expr, global_symbol_table),
-               "int"))) {
+  // if (switch_->enum_expr->type == EXPR_INTEGER ||
+  // (switch_->enum_expr->type == EXPR_IDENTIFIER &&
+  //  !strcmp(get_ast_expr_type(switch_->enum_expr, global_symbol_table),
+  //  "int")))
+  char const*switch_expr_type = get_ast_expr_type(switch_->enum_expr, global_symbol_table);
+  if (strcmp(switch_expr_type, "int")==0){
 
     int label_id = *id;
     // SWITCH prologue
@@ -66,7 +69,8 @@ void walk_ast_switch_t(ast_switch_t const *switch_, int *id) {
       push_scope(&global_symbol_table);
       walk_ast_block_t(switch_->cases[i]->body, id);
       last_expr = get_last_block_expr(switch_->cases[i]->body);
-      if (last_expr == NULL || strcmp(switch_return_type,
+      if (last_expr == NULL ||
+          strcmp(switch_return_type,
                  get_ast_expr_type(last_expr, child_scope))) {
         REPORT_ERROR("Error: switch case return type mismatch\n");
         exit(1);
@@ -77,9 +81,9 @@ void walk_ast_switch_t(ast_switch_t const *switch_, int *id) {
       GEN_INSTRUCTIONS("\tJMP _switch_end_%d_\n", label_id);
     }
     GEN_INSTRUCTIONS("_switch_end_%d_:\n", label_id);
-
-  } else {
-    REPORT_ERROR("Error: switch expression must be integer\n");
+  }
+  else {
+    REPORT_ERROR("Error: switch expression must be int,found %s\n",switch_expr_type);
     exit(1);
   }
 }
